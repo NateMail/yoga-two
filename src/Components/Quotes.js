ba6aca0282313e59fe31d366cd5d672e17393523
Nathan Mailhiot <nathanjm12@outlook.com>
2021-02-02 14:25:47 -0500

    Typeo

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -3,7 +3,7 @@ import React from "react";
 const Quotes = () => {
   return (
     <div className="quotes">
-      <h1 className="quotes__text">Quotes Here</h1>
+      <h1 className="quotes__text">Quotes Go Here</h1>
     </div>
   );
 };
