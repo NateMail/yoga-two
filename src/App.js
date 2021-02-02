6b9e9f61c52da1ab2b4a775312c43494e3a4d666
Nathan Mailhiot <nathanjm12@outlook.com>
2021-02-02 13:32:01 -0500

    New look with nav

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Nav from "./Components/Nav";
 import AnimatedSwitch from "./Components/AnimatedSwitch";
+import Quotes from "./Components/Quotes";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
     <div className="App">
+      <Quotes />
       <BrowserRouter>
         <Nav />
         <div className="page-container">
           <AnimatedSwitch />
         </div>
       </BrowserRouter>
+      <Footer />
     </div>
   );
 };
