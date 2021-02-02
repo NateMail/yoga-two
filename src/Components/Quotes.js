28038ae3f49282fc2f72a7ebcf68e0597740e31e
Nathan Mailhiot <nathanjm12@outlook.com>
2021-02-02 15:05:40 -0500

    Quotes background

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Quotes = () => {
   return (
-    <div className="quotes">
-      <h1 className="quotes__text">Quotes Go Here</h1>
+    <div className="cover">
+      <div className="quotes">
+        <h1 className="quotes__text">Quotes Go Here</h1>
+      </div>
     </div>
   );
 };
